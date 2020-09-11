3204ea790e57e0093c93345fe0957360f7595814
Steven Shaw <steven@steshaw.org>
2020-09-12 09:58:34 +1000

    Use lnl7 plus buildLayeredImage

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
-FROM nixos/nix:2.3.6
+FROM lnl7/nix:2.3.6
 
 RUN nix-env -iA \
   nixpkgs.docker \
   nixpkgs.gnutar \
+  nixpkgs.gzip \
--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 { pkgs ? import <nixpkgs> { } }:
 
-pkgs.dockerTools.buildImage {
+pkgs.dockerTools.buildLayeredImage {
   name = "hello-docker";
   config = {
     Cmd = [ "${pkgs.hello}/bin/hello" ];
